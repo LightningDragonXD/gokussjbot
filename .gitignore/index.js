83bf3224a130c3abb589ffc535a39c6d157573c8
LightningDragonXD <jacquesdu59@gmail.com>
2018-05-13 19:06:48 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,16 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+var prefix = "<G>";
 bot.on('ready', function(){
   bot.user.setGame("DBZ, <G>help");
+});
+bot.on('message', function(message){
+   if(message.content === (prefix+"bonjour")){
+      message.reply("Bonjour à toi");
+        var reveille = ["image/bonjour.gif","image/bonjour2.gif","image/bonjour3.gif"];
+        var randomr = Math.floor(Math.random()*reveille.length);
+        return message.channel.sendFile(reveille[randomr]);
+   }
 });
 bot.login(process.env.TOKEN);
