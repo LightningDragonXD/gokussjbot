520a57b4512571207d5271b95c0f0d116b584ec6
LightningDragonXD <jacquesdu59@gmail.com>
2018-05-14 22:58:21 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 var mention = "<@401421641097412608>";
-const prefix = "G";
+const prefix = "<G>";
 bot.on('ready', function(){
-  bot.user.setGame("DBZ, Ghelp");
+  bot.user.setGame("DBZ, <G>help");
 });
 var rep = ["je joue","je parle","j'écoute de la musique","je regarde un anime"];
 var phrase = ["Tu suces ? MDR", "Tu pues du cul !","Tu es un trans ? XD","Askip tu as un pc en carton :p","Si y a ton ex qui te dit : tu retrouvera jamais quelqu'un comme moi . dit lui : heureusement","T'es comme France 2 personne te regarde.","Tu ne m'arrives pas à la cheville mais si un jour tu l'atteints, soit gentille et fais moi mes lacets.","Commence par t'occuper de ton cul, au vue de la superficie cela devrait t'occuper longtemps !","-Tu sais ce qui est bien chez toi?\n-Nan\n-Bah moi non plus","- Tu viens à ma soirée fruits de mer et poissons ?\n- Oui avec plaisir\n- Tant mieux j'avais besoin d'un thon","Ta bouche c'est comme une porte, ça se ferme!" ];
@@ -17,7 +17,7 @@ function sendError(message, description){
 bot.on('message', message => {
   if(message.content[0] === prefix){
   let splitM = message.content.split(" ");
-  if(splitM[0] === 'Ghelp'){
+  if(splitM[0] === '<G>help'){
     if(splitM.length === 1){
        var embed = new Discord.RichEmbed();
             embed.setTitle('Help')
@@ -31,7 +31,7 @@ bot.on('message', message => {
     }else{
         sendError(message,"Erreur, problèmes dans les paramètres.");
     }
-  }else if(splitM[0] === 'Gbonjour'){
+  }else if(splitM[0] === '<G>bonjour'){
       message.reply("Bonjour à toi");
         var reveille = ["image/bonjour.gif","image/bonjour2.gif","image/bonjour3.gif"];
         var randomr = Math.floor(Math.random()*reveille.length);
