e62218441ef2b65bea50f599004e3e403fb1fe96
LightningDragonXD <jacquesdu59@gmail.com>
2018-05-14 22:43:56 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -7,9 +7,18 @@ bot.on('ready', function(){
 });
 var rep = ["je joue","je parle","j'écoute de la musique","je regarde un anime"];
 var phrase = ["Tu suces ? MDR", "Tu pues du cul !","Tu es un trans ? XD","Askip tu as un pc en carton :p","Si y a ton ex qui te dit : tu retrouvera jamais quelqu'un comme moi . dit lui : heureusement","T'es comme France 2 personne te regarde.","Tu ne m'arrives pas à la cheville mais si un jour tu l'atteints, soit gentille et fais moi mes lacets.","Commence par t'occuper de ton cul, au vue de la superficie cela devrait t'occuper longtemps !","-Tu sais ce qui est bien chez toi?\n-Nan\n-Bah moi non plus","- Tu viens à ma soirée fruits de mer et poissons ?\n- Oui avec plaisir\n- Tant mieux j'avais besoin d'un thon","Ta bouche c'est comme une porte, ça se ferme!" ];
+function sendError(message, description){
+   var embed = new Discord.RichEmbed()
+            .setColor('#FE0000')
+            .setDescription(':x: '+ description)
+  message.channel.send(embed);
+}
+
 bot.on('message', message =>{
-  const msg = message.content;
-  if(msg === (prefix+"help")){
+  if(message.content[0] === prefix){
+  let splitM = message.content.split(" ");
+  if(splitM[0] === (prefix+"help")){
+    if(splitM.length === 1){
        var embed = new Discord.RichEmbed();
             embed.setTitle('Help')
             .setAuthor('Goku', 'https://i.imgur.com/2vSM4o1.png')
@@ -19,41 +28,45 @@ bot.on('message', message =>{
             .setFooter('Créer par Goku', 'https://i.imgur.com/2vSM4o1.png')     
             .setColor("#0155FE")
        message.channel.sendEmbed(embed);
+    }else{
+        sendError(message,"Erreur, problèmes dans les paramètres.");
+    }
   }
-   if(msg === (prefix+"bonjour")){
+   if(splitM[0] === (prefix+"bonjour")){
       message.reply("Bonjour à toi");
         var reveille = ["image/bonjour.gif","image/bonjour2.gif","image/bonjour3.gif"];
         var randomr = Math.floor(Math.random()*reveille.length);
         return message.channel.sendFile(reveille[randomr]);
    }
-  if(msg === (prefix+"ça va")){
+  if(splitM[0] === (prefix+"ça va")){
         message.reply("Oui et toi ?");
    }
-  if(msg === (prefix+"oui")){
+  if(splitM[0] === (prefix+"oui")){
         message.reply("Tant mieux :blush:");
    }
-  if(msg === (prefix+"tu fais quoi ?")){
+  if(splitM[0] === (prefix+"tu fais quoi ?")){
         message.reply("J'emmerde les gens :smile: et toi ?");
    }
-  if(msg === (prefix+rep[0]) || msg === (prefix+rep[1]) || msg === (prefix+rep[2]) || msg === (prefix+rep[3])){
+  if(splitM[0] === (prefix+rep[0]) || splitM[0] === (prefix+rep[1]) || splitM[0] === (prefix+rep[2]) || splitM[0] === (prefix+rep[3])){
         message.reply("Cool :blush:");
    }
-  if(msg === (prefix+"rien")){
+  if(splitM[0] === (prefix+"rien")){
         message.reply("Attends, j'arrive !");
    }
-  if(msg === (prefix+"amuse toi bien =3")){
+  if(splitM[0] === (prefix+"amuse toi bien =3")){
         message.reply("Merci, toi aussi !");
   }
-  if(msg === (prefix+"botname")){
+  if(splitM[0] === (prefix+"botname")){
         bot.user.setUsername(message.substr(9)); 
   }
-  if(msg === (prefix+"XD")){
+  if(splitM[0] === (prefix+"XD")){
     let membre = message.guild.member(message.mentions.users.first())
     if(membre){ 
     var r = Math.floor(Math.random()*phrase.length);
       return message.channel.sendMessage(phrase[r]);
     }
   }
+}
 
 });
   bot.on('guildMemberAdd', member =>{
